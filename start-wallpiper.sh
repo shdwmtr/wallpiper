4bb10066f54e4eb8bcc39bb0382e84ca3f487aaf
Ethan Alexander <git@ethanjalexander.ca>
2026-08-21 22:10:28 -0300

    chore: update gitignore

--- start-wallpiper.sh
+++
@@ -1,8 +0,0 @@
-make build-core
-make build-hyprland
-
-export WALLPIPER_PROTON_BIN="/home/shdw/.local/share/Steam/steamapps/common/Proton 11.0/proton"
-export WALLPIPER_PORTAL=hyprland
-export WALLPIPER_WPE_IPC_DUMP_FILE=$HOME/file.log
-
-exec ./target/release/wallpiper-daemon
